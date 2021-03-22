1bada9da5657c9e6cd24e7ebae353997f7a1bb67
Ricky Almeida <ricky.almeida@nave.rs>
2021-03-22 17:04:40 -0300

    refactor(app): create Content and SideBar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+import '../styles/sidebar.scss'
+
+type Genre = {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
+}
+
+type Props = {
+  genres: Genre[]
+  selectedGenreId: number
+  onSelectGenre: (id: number) => void
+}
+
+export function SideBar({ genres, selectedGenreId, onSelectGenre }: Props): JSX.Element {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
